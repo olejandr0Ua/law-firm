b609a96db57267b5027dfd1aa361758131f47666
olejandr0Ua <sanya3776103594@gmail.com>
2026-04-05 17:34:14 +0300

    Updated hero section styles

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // Вказуємо відносні шляхи для коректної роботи після збірки
   base: './', 
   build: {
     outDir: 'dist',
-    minify: 'terser', // або esbuild (за замовчуванням)
+    minify: 'terser', 
   }
 });
